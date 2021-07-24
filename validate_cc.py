e6bb1ea290bbe49232f175a92597dbbf0a0069bd
Raymond Munian <raymond.munian@gmail.com>
2021-07-24 16:05:42 -0400

    Update validate_cc.py
    Update to a single regex

--- validate_cc.py
+++ validate_cc.py
@@ -1,24 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 import re
 import sys
 
-#Verifies the line matches  a pattern
-=
-formatted_num = re.compile(r'[4-6]\d{3}-?(\d{4}-?){3}')
-has_repeat = re.compile(r"(\d)\1{3,}")
-line_num=0
+validator = re.compile(r'^(?!.*(\d)(-?\1){3})[4-6]\d{3}-?((\d){4}-?){3}$')
 num_lines=0
 for line in sys.stdin:
     if num_lines==0:
         num_lines=int(line)
     else:
-        matched_format = formatted_num.match(line)
+        matched_format = validator.match(line)
         if (matched_format is not None):
-            line = line.replace('-','')
-            match_repeat = has_repeat.search(line)
-            if (match_repeat==None):
-                print('Valid')                
-            else:
-                print('Invalid')
+            print('Valid')                
         else:
             print('Invalid')
         num_lines=num_lines-1
